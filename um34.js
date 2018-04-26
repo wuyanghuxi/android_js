05cd5cfc953acf9bd295eac1e52fa41739b45c24
wuyanghuxi <34669761+wuyanghuxi@users.noreply.github.com>
2018-04-26 11:41:39 +0800

    Delete um34.js

--- um34.js
+++
@@ -1,8 +0,0 @@
-{
-  "update_url"      : "http://www.ruidengkeji.com/apk/UM24C_V1.5.5.apk",
-  "update_content"  : "new version needs to be updated",
-  "update_ver_code" : 1,
-  "update_ver_name" : "1.5.5",
-  "ignore_able"     : true,
-  "md5"             : "211117732C307BEA1188965E6CA41443"
-}
