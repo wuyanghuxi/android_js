2bb6d5cc244eee32645493bec7ee3eb0f2361483
wuyanghuxi <34669761+wuyanghuxi@users.noreply.github.com>
2018-04-26 11:37:34 +0800

    Update um34.js

--- um34.js
+++ um34.js
@@ -1,8 +1,8 @@
 {
   "update_url"      : "http://www.ruidengkeji.com/apk/UM24C_V1.5.5.apk",
-  "update_content"  : "你有新的版本需要更新",
+  "update_content"  : "new version needs to be updated",
   "update_ver_code" : 1,
-  "update_ver_name" : "1.1.5",
+  "update_ver_name" : "1.5.5",
   "ignore_able"     : true,
   "md5"             : "211117732C307BEA1188965E6CA41443"
 }
